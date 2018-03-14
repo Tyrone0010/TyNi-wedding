7bf3afbc4d41dbe52e88b46107d15e7b7c86773b
Tyrone Fredrickson <Tyrone.Fredrickson@iea-hamburg.de>
2018-03-14 20:21:20 +0000

    initial push

---
+++ TyNi.Wedding.AuthApi/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TyNi.Wedding.AuthApi
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext() : base("AuthContext")
+        {
+            
+        }
+
+    }
+}
---
+++ TyNi.Wedding.AuthApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using TyNi.AuthApi;
+using TyNi.Wedding.AuthApi.Providers;
+
+[assembly: OwinStartup(typeof(TyNi.Wedding.AuthApi.Startup))]
+namespace TyNi.Wedding.AuthApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions oAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(oAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+}
